51df0ebee59d8d03640a7e91ca65b3e98f27cd08
Vadim Zinoviev <vazinoviev2107@gmail.com>
2020-06-21 22:39:37 +0300

    second commit countdown

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -1,7 +1,14 @@
 const seconds = document.getElementById("timer");
+// const time = document.getElementById("timer");
+// const dd = new Date();
+// const hh = dd.getHours();
+// const mm = dd.getMinutes();
+// const ss = dd.getSeconds();
+// const padNum = ((num) => num.toString(). padStart (2 , "0"));
 
 const timer = function(){
     seconds.textContent -= 1;
+    // time.textContent = `${padNum(hh)}:${padNum(mm)}:${padNum(ss)}`;
 
     if (seconds.textContent == 0) {
     alert('Вы победили в конкурсе!');    
