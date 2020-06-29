181700394a34805fa04df9e185d9e965419428b6
Vadim Zinoviev <vazinoviev2107@gmail.com>
2020-06-29 22:45:45 +0300

    first commit interests

---
+++ html-forms/interests/task.js
@@ -0,0 +1,19 @@
+const interestCheck = document.querySelectorAll('.interest__check');
+let arrInterestCheck = Array.from(interestCheck);
+
+arrInterestCheck.forEach(elem => {
+    let interestClosest = elem.closest('.interest'); //родитель инпута
+    const arrInterest = Array.from(interestClosest.querySelectorAll('.interest__check'));
+    
+	if(interestClosest.querySelector('.interests')) { //если у родителя инпута есть вложенный список
+        elem.addEventListener('change', () => { //добавляем на него событие 'change'
+            for (let i = 0; i < arrInterest.length; i++) {
+                if(elem.checked) { //если на элементе галочка                   
+                    arrInterest[i].checked = true;
+                } else {                   
+                    arrInterest[i].checked = false;
+                }
+            }
+		});
+	}
+})
