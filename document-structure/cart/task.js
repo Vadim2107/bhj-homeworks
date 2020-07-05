ee2a767b3ade567234ec061b938cfb757abb188b
Vadim Zinoviev <vazinoviev2107@gmail.com>
2020-07-05 20:14:20 +0300

    third commit cart

--- document-structure/cart/task.js
+++ document-structure/cart/task.js
@@ -4,11 +4,8 @@ const productQuantityControls = document.querySelectorAll('.product__quantity-co
 const product = document.querySelectorAll('.product');
 const cartProducts = document.querySelector('.cart__products');
 
-// let arrCartProduct = [];
 let arrControl = Array.from(productQuantityControls);
 let arrProduct = Array.from(product);
-
-
 
 for (let i = 0; i < arrControl.length; i++) {
     arrControl[i].addEventListener('click', (event) => {
@@ -28,27 +25,31 @@ for (let i = 0; i < arrControl.length; i++) {
 
 for (let i = 0; i < arrProduct.length; i++) {
     arrProduct[i].addEventListener('click', (event) => {
-        const cartProduct = cartProducts.querySelectorAll('cart__product');
-        let arrCartProduct = Array.from(cartProduct);
+        const cartProduct = document.querySelectorAll('.cart__product');
+        let arrCartProduct = Array.from(cartProduct);        
 
-        const id = arrProduct[i].getAttribute('data-id');
-        const value = arrProduct[i].querySelector('.product__quantity-value');
+        const id = arrProduct[i].dataset.id;
+        const value = Number(arrProduct[i].querySelector('.product__quantity-value').innerText);
         const img = arrProduct[i].querySelector('.product__image').getAttribute('src');
         
-        // arrCartProduct.find((item) => {
-        //     if (item.getAttribute('data-id')) {
-        //         alert('click');
-        //     } else
-             if (event.target.matches('.product__add')) {
-                    cartProducts.insertAdjacentHTML('beforeEnd', `
-                    <div class="cart__product" data-id="${arrProduct[i].dataset.id}">
-                        <img class="cart__product-image" src="${img}">
-                        <div class="cart__product-count">${value.innerText}</div>
-                    </div>`);
-
-                    // arrCartProduct.push(cart);           
+        const elementParent = arrCartProduct.find((elem) => {
+            return elem.dataset.id === id;
+            // return elem.dataset.id === event.target.closest('.product').dataset.id;
+        })            
+            
+        if (elementParent) {
+            if (event.target.matches('.product__add')) {
+                let valueAdd = Number(elementParent.querySelector('.cart__product-count').innerText);
+                elementParent.querySelector('.cart__product-count').innerText = valueAdd + value;
+            }            
+        } else {
+            if (event.target.matches('.product__add')) {
+                cartProducts.insertAdjacentHTML('beforeEnd', `
+                <div class="cart__product" data-id="${id}">
+                    <img class="cart__product-image" src="${img}">
+                    <div class="cart__product-count">${value}</div>
+                </div>`);
             }
-            return arrCartProduct;
-        // })        
+        }
     })
 }
