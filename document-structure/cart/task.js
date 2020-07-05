633109e610a9151a3ac799608ced7c82e2990575
Vadim Zinoviev <vazinoviev2107@gmail.com>
2020-07-05 17:23:04 +0300

    second commit cart

--- document-structure/cart/task.js
+++ document-structure/cart/task.js
@@ -3,8 +3,12 @@
 const productQuantityControls = document.querySelectorAll('.product__quantity-controls');
 const product = document.querySelectorAll('.product');
 const cartProducts = document.querySelector('.cart__products');
+
+// let arrCartProduct = [];
 let arrControl = Array.from(productQuantityControls);
 let arrProduct = Array.from(product);
+
+
 
 for (let i = 0; i < arrControl.length; i++) {
     arrControl[i].addEventListener('click', (event) => {
@@ -24,18 +28,27 @@ for (let i = 0; i < arrControl.length; i++) {
 
 for (let i = 0; i < arrProduct.length; i++) {
     arrProduct[i].addEventListener('click', (event) => {
-        let value = arrProduct[i].querySelector('.product__quantity-value');
-        
-        if (event.target.matches('.product__add')) {
-            cartProducts.insertAdjacentHTML('beforeEnd', `
-            <div class="cart__product" data-id="${arrProduct[i].dataset.id}">
-                <img class="cart__product-image" src="https://static-eu.insales.ru/images/products/1/7875/257179331/4515850.jpg">
-                <div class="cart__product-count">${value.innerText}</div>
-            </div>`);
+        const cartProduct = cartProducts.querySelectorAll('cart__product');
+        let arrCartProduct = Array.from(cartProduct);
 
-            // alert('click');
-        }
-
+        const id = arrProduct[i].getAttribute('data-id');
+        const value = arrProduct[i].querySelector('.product__quantity-value');
+        const img = arrProduct[i].querySelector('.product__image').getAttribute('src');
         
+        // arrCartProduct.find((item) => {
+        //     if (item.getAttribute('data-id')) {
+        //         alert('click');
+        //     } else
+             if (event.target.matches('.product__add')) {
+                    cartProducts.insertAdjacentHTML('beforeEnd', `
+                    <div class="cart__product" data-id="${arrProduct[i].dataset.id}">
+                        <img class="cart__product-image" src="${img}">
+                        <div class="cart__product-count">${value.innerText}</div>
+                    </div>`);
+
+                    // arrCartProduct.push(cart);           
+            }
+            return arrCartProduct;
+        // })        
     })
 }
