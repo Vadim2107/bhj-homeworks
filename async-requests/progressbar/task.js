debecfe018e3b1b6d868762191c07889836d5870
Vadim Zinoviev <vazinoviev2107@gmail.com>
2020-07-08 20:32:53 +0300

    first commit progressbar

--- async-requests/progressbar/task.js
+++ async-requests/progressbar/task.js
@@ -7,20 +7,10 @@ form.addEventListener('submit', (e) => {
     const formData = new FormData(form);
     let xhr  = new XMLHttpRequest();
     xhr.open('POST', 'https://netology-slow-rest.herokuapp.com/upload.php');
-    xhr.upload.onprogress = (e) => {
-        // alert(e.loaded);
-        // alert(e.total);
-        progress.value = e.loaded / e.total;
-    }
-
-    // xhr.addEventListener('readystatechange', function() {
-
-    //     if (this.readyState == request.DONE && this.status == 200) {
-    //         let data = JSON.parse(this.responseText);
-
-    //     }   
-    // });
+    xhr.upload.onprogress = (event) => {        
+        progress.value = event.loaded / event.total;
+    }    
 
     xhr.send(formData);
-    e.preventDefault();
+    e.preventDefault();    
 });
