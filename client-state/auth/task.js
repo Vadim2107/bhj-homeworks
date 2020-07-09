2661ab93bc3a2f4d2e439edc837ef176ef78b168
Vadim Zinoviev <vazinoviev2107@gmail.com>
2020-07-09 22:17:29 +0300

    first commit auth

--- client-state/auth/task.js
+++ client-state/auth/task.js
@@ -6,17 +6,14 @@ const signinBtn = document.getElementById('signin__btn');
 
 signin.classList.add('signin_active');
 
-// signinForm.addEventListener('submit', (e) => {
-    
+signinForm.addEventListener('submit', () => {    
 
-    signinBtn.addEventListener('click', () => {
+    signinBtn.addEventListener('click', (e) => {
         const formData = new FormData(signinForm);
         let xhr  = new XMLHttpRequest();
         xhr.open('POST', 'https://netology-slow-rest.herokuapp.com/auth.php');
     
         xhr.send(formData);
         e.preventDefault();
-    })
-    
-    // signin.classList.add('signin_active');   
-// });
+    })       
+});
