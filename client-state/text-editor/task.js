61e00bd710f194c3957313e97f07fb6efcdb02a6
Vadim2107 <58651870+Vadim2107@users.noreply.github.com>
2020-07-11 19:08:05 +0300

    second commit text-editor

--- client-state/text-editor/task.js
+++ client-state/text-editor/task.js
@@ -3,16 +3,18 @@
 const editor = document.getElementById('editor');
 
 
-document.addEventListener('keyup', () => {
+document.addEventListener('input', () => {
     if (editor.value.length > 0) {
         // localStorage.setItem('editor', `${editor.value}`);        
         localStorage.editor = `${editor.value}`;
+    } else if (editor.value.length == 0) {
+        localStorage.editor = ``;
     }
 })
 
-
-
-// editor.value = localStorage.getItem('editor');
-editor.value = localStorage.editor;
-
- 
+if (localStorage.editor == undefined) {
+    editor.value = '';
+} else {
+    // editor.value = localStorage.getItem('editor');
+    editor.value = localStorage.editor;
+}
