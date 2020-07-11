eabbdafb2ad66cd054074092760f1b02557acea1
Vadim2107 <58651870+Vadim2107@users.noreply.github.com>
2020-07-11 19:06:09 +0300

    Update task.js

--- client-state/popup/task.js
+++ client-state/popup/task.js
@@ -2,11 +2,25 @@
 
 const subscribeModal = document.getElementById('subscribe-modal');
 const modalClose = document.querySelector('.modal__close');
+const getCookie = (name) => {
+    const value = "; " + document.cookie;
+    let parts = value.split("; " + name + "=");
+    if (parts.length === 2) {
+        return parts
+        .pop()
+        .split(";")
+        .shift();
+    }
+}
 
-subscribeModal.classList.add('modal_active');
+if (getCookie('modalWindow') == undefined) {
+    subscribeModal.classList.add('modal_active');
+} else {
+    subscribeModal.classList.remove('modal_active');
+}
 
 modalClose.addEventListener('click', () => {
-    let close = subscribeModal.classList.remove('modal_active');
-    document.cookie = 'modalClose=ОкноЗакрыто';
-    // document.cookie = 'Close=' + encodeURIComponent('Окно закрыто');
+    subscribeModal.classList.remove('modal_active');
+    // document.cookie = 'modalWindow=windowIsClosed';
+    document.cookie = 'modalWindow=' + encodeURIComponent('The modal window is closed');
 })
